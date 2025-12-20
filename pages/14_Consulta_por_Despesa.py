dea4ebc4f36f6211fe2bb2dd1422639a900fa249
planejamentobarbacena-web <planejamento.barbacena@gmail.com>
2025-12-20 18:06:38 -0300

    Update 14_Consulta_por_Despesa.py

--- pages/14_Consulta_por_Despesa.py
+++ pages/14_Consulta_por_Despesa.py
@@ -149,17 +149,29 @@ graf = (
         ["empenhado_liquido", "saldoBaixado"],
         as_=["Tipo", "Valor"]
     )
-    .mark_bar(size=40)
+    .mark_bar(size=26)  # barras mais finas
     .encode(
-        x=alt.X("anoEmpenho:N", title="Exercício"),
-        xOffset="Tipo:N",
-        y=alt.Y("Valor:Q", title="Valor (R$)"),
+        x=alt.X(
+            "anoEmpenho:N",
+            title="Exercício",
+            axis=alt.Axis(labelAngle=0)
+        ),
+        xOffset=alt.XOffset("Tipo:N"),
+        y=alt.Y(
+            "Valor:Q",
+            title="Valor (R$)"
+        ),
         color=alt.Color(
             "Tipo:N",
             title="Tipo",
             scale=alt.Scale(
                 domain=["empenhado_liquido", "saldoBaixado"],
                 range=["#1f77b4", "#ff7f0e"]
+            ),
+            legend=alt.Legend(
+                orient="bottom",
+                direction="horizontal",
+                columns=2   # 👈 só dois tipos
             )
         ),
         tooltip=[
@@ -172,6 +184,7 @@ graf = (
 )
 
 st.altair_chart(graf, use_container_width=True)
+
 
 # =======================
 # TABELA
