5c43f3c2c0154369b5f1d3a8878e72ff953a43c4
planejamentobarbacena-web <planejamento.barbacena@gmail.com>
2025-12-23 15:51:43 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -156,7 +156,7 @@ st.markdown("### 📊 Composição do Empenhado por Exercício")
 
 graf = (
     alt.Chart(df_long)
-    .mark_bar(size=80)  # 👈 largura ajustada (desktop + mobile)
+    .mark_bar(size=60)  # 👈 largura ajustada 
     .encode(
         x=alt.X(
             "anoEmpenho:N",
